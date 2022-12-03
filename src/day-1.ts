946820ce196a21ff185c6aa4a99eb90c74fe3add
Christopher Oldfield <coldfield@salesforce.com>
2022-12-03 11:30:56 +1100

    Day 1 - Part 2

--- src/day-1.ts
+++ src/day-1.ts
@@ -8,22 +8,18 @@ const __dirname = path.dirname(__filename);
 const inputPath = `${__dirname}/../input/day-1.txt`;
 const input = readFileSync(inputPath, 'utf8');
 
-const elves = [];
+const elves: number[] = [];
 let currentElfIndex = 0;
-let biggestElfIndex = 0;
 
 input.split('\n').forEach((line) => {
-  if (!line.length) {
-    if (elves[currentElfIndex] > elves[biggestElfIndex]) {
-      biggestElfIndex = currentElfIndex;
-    }
-
-    return currentElfIndex++;
-  }
-
+  if (!line.length) return currentElfIndex++;
   if (!elves[currentElfIndex]) elves[currentElfIndex] = 0;
   elves[currentElfIndex] += +line;
 });
 
-const mostCalories = elves[biggestElfIndex];
-console.log(`The elf carrying the most calories is carrying ${mostCalories} calories`);
+const sortedElves = elves.sort((a, b) => b - a);
+const topThree = sortedElves.slice(0, 3);
+const totalCalories = topThree.reduce((runningTotal, elf) => runningTotal + elf, 0);
+
+console.log(`The elf carrying the most calories is carrying ${sortedElves[0]} calories`);
+console.log(`The total calories carried by the top three elves is ${totalCalories} calories`);
