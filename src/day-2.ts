849cc927fb959a65612f90e242bed3abd11191fb
Christopher Oldfield <coldfield@salesforce.com>
2022-12-08 11:17:40 +1100

    Combine both parts of Day 2 into one solution

--- src/day-2.ts
+++ src/day-2.ts
@@ -8,14 +8,17 @@ class MatchResult {
   constructor(key: string) {
     switch (key) {
       case 'X':
+      case 'Loss':
         this.result = 'Loss';
         this.value = 0;
         return;
       case 'Y':
+      case 'Draw':
         this.result = 'Draw';
         this.value = 3;
         return;
       case 'Z':
+      case 'Victory':
         this.result = 'Victory';
         this.value = 6;
     }
@@ -29,20 +32,31 @@ class HandShape {
   constructor(key: string) {
     switch (key) {
       case 'A':
+      case 'X':
       case 'Rock':
         this.shape = 'Rock';
         this.value = 1;
         return;
       case 'B':
+      case 'Y':
       case 'Paper':
         this.shape = 'Paper';
         this.value = 2;
         return;
       case 'C':
+      case 'Z':
       case 'Scissors':
         this.shape = 'Scissors';
         this.value = 3;
     }
+  }
+
+  challenge(opponent: HandShape) {
+    if (this.shape === opponent.shape) return new MatchResult('Draw');
+    if ((this.shape === 'Rock' && opponent.shape === 'Scissors') ||
+        (this.shape === 'Scissors' && opponent.shape === 'Paper') ||
+        (this.shape === 'Paper' && opponent.shape === 'Rock')) return new MatchResult('Victory');
+    return new MatchResult('Loss');
   }
 
   forceResult(result: MatchResult) {
@@ -62,17 +76,24 @@ class HandShape {
   }
 }
 
+let part1Score = 0;
 
-let totalScore = 0;
+input.forEach((line) => {
+  const [opponent, player] = line.split(' ').map((key) => new HandShape(key));
+  const result = player.challenge(opponent);
+  part1Score += result.value + player.value;
+});
+
+console.log(`The final score for part 1 is ${part1Score}`);
+
+let part2Score = 0;
 
 input.forEach((line) => {
   const codes = line.split(' ');
   const opponent = new HandShape(codes[0]);
   const result = new MatchResult(codes[1]);
   const move = opponent.forceResult(result);
-
-  console.log(`The opponent played ${opponent.shape}. To force a ${result.result} you should play ${move.shape}.`);
-  totalScore += result.value + move.value;
+  part2Score += result.value + move.value;
 });
 
-console.log(`The final score is ${totalScore}`);
+console.log(`The final score for part 2 is ${part2Score}`);
