f91a87e2e33cde8a55b22c23571443c4f61d3230
Christopher Oldfield <coldfield@salesforce.com>
2022-12-08 18:32:37 +1100

    Day 8-2

--- src/day-8.ts
+++ src/day-8.ts
@@ -14,34 +14,57 @@ const isDirectionVisible = (trees: number[], maxHeight: number) => {
     true);
 };
 
+const getColumn = (x: number) => grid.map((row) => row[x]);
+const getUpTrees = (x: number, y: number) => getColumn(x).slice(0, y).reverse();
+const getDownTrees = (x: number, y: number) => getColumn(x).slice(y + 1);
+const getLeftTrees = (x: number, y: number) => grid[y].slice(0, x).reverse();
+const getRightTrees = (x: number, y: number) => grid[y].slice(x + 1);
+
 const isTreeVisible = (x: number, y: number) => {
   if (x === 0 || x === xMax ||
       y === 0 || y === yMax) return true;
 
   const tree = grid[y][x];
 
-  const left = grid[y].slice(0, x);
+  const left = getLeftTrees(x, y);
   if (isDirectionVisible(left, tree)) return true;
 
-  const right = grid[y].slice(x + 1);
+  const right = getRightTrees(x, y);
   if (isDirectionVisible(right, tree)) return true;
 
-  const column = grid.map((row) => row[x]);
-  const up = column.slice(0, y);
+  const up = getUpTrees(x, y);
   if (isDirectionVisible(up, tree)) return true;
 
-  const down = column.slice(y + 1);
+  const down = getDownTrees(x, y);
   if (isDirectionVisible(down, tree)) return true;
 
   return false;
 };
 
+const getDirectionScore = (trees: number[], maxHeight: number) => {
+  const firstBlocker = trees.findIndex((tree) => tree >= maxHeight);
+  return firstBlocker === -1 ? trees.length : firstBlocker + 1;
+}
+
+const getScenicScore = (x: number, y: number) => {
+  const tree = grid[y][x];
+  return getDirectionScore(getUpTrees(x, y), tree) *
+    getDirectionScore(getDownTrees(x, y), tree) *
+    getDirectionScore(getLeftTrees(x, y), tree) *
+    getDirectionScore(getRightTrees(x, y), tree);
+};
+
 let visibleCount = 0;
+let highestScenicScore = 0;
 
 for (let y = 0; y < yMax; y++) {
   for (let x = 0; x < xMax; x++) {
     if (isTreeVisible(x, y)) visibleCount++;
+
+    const scenicScore = getScenicScore(x, y);
+    if (scenicScore > highestScenicScore) highestScenicScore = scenicScore;
   }
 }
 
 console.log(`The number of trees that are visible are ${visibleCount}`);
+console.log(`The highest scenic score possible is ${highestScenicScore}`);
