2c59fd400515624e18f5934ec9a63b5c07b5c5fc
Christopher Oldfield <coldfield@salesforce.com>
2022-12-23 18:07:47 +1100

    Day 13-2

--- src/day-13.ts
+++ src/day-13.ts
@@ -48,3 +48,13 @@ for (let i = 0; i < input.length; i += 2) {
 }
 
 console.log(`The sum of the indices of the ordered pairs is ${indexTotal}`);
+
+const dividerPackets = ['[[2]]', '[[6]]'];
+const sorted = [...input, ...dividerPackets].sort((a, b) =>
+  checkOrder(JSON.parse(a), JSON.parse(b)) ? -1 : 1
+);
+
+const firstDivider = sorted.indexOf('[[2]]') + 1;
+const secondDivider = sorted.indexOf('[[6]]') + 1;
+const decoderKey = firstDivider * secondDivider;
+console.log(`The decoder key for the distress signal is ${decoderKey}`);
